9e40fe5350d227374a29410c0cc38dd93b8d80fa
shafi <shafi.bahrami.2015@gmail.com>
2023-10-08 17:32:33 +0200

    setting pages

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,77 @@
+import { prisma } from "@/lib/db/prisma"
+import {redirect} from 'next/navigation';
+
+export const metadata={
+   title:'Add product'
+}
+
+async function addProduct(formData:FormData){
+   "use server"
+
+   //getting data from server action
+   const name=formData.get('name')?.toString();
+   const description=formData.get('description')?.toString();
+   const imageUrl=formData.get('imageUrl')?.toString();
+   const price=Number(formData.get('price') || 0);
+ 
+   //validating the data
+   if(!name || !description || !imageUrl || !price){
+      throw Error('Field missing');
+   }
+
+   //saving data 
+   await prisma.product.create({
+      data:{
+         name,
+         description,
+         imageUrl,
+         price
+      }
+   });
+
+   //redirecting to the homepage
+   redirect('/');
+}
+
+export default function AddProductPage(){
+   return (
+      <div>
+         <h1 className="font-bold mb-3 text-lg">Add product</h1>
+         <form action={addProduct}>
+            <input
+               required
+               type="text"
+               name="name"
+               className="input input-bordered w-full mb-3"
+               placeholder="Product name"
+            />
+            <textarea
+               required
+               className="textarea textarea-bordered w-full mb-3"
+               name="description"
+               placeholder="Description"
+            />
+            <input
+               required
+               className="input input-bordered w-full mb-3"
+               name="imageUrl"
+               placeholder="image URL"
+               type="url"
+            />
+            <input
+               required
+               className="input input-bordered w-full mb-3"
+               name="price"
+               placeholder="Price"
+               type="number"
+            />
+            <button
+               type="submit"
+               className="btn btn-primary btn-block"
+            > 
+               Add product
+            </button>
+         </form>
+      </div>
+   )
+}
