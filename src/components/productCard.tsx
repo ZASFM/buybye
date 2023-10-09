16dfdc8640ad4ab7268885f4c85b1c3c7c6b8aa0
shafi <shafi.bahrami.2015@gmail.com>
2023-10-09 15:43:48 +0200

    products

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -8,6 +8,9 @@ interface ProductCardProps{
 }
 
 export default function ProductCard({product}:ProductCardProps){
+   //if a product is less that 7 days old
+   const isNew = Date.now() - new Date(product.createdAt).getTime() > 1000*60*60*24*7
+
    return (
       <div>
          <Link
@@ -20,11 +23,13 @@ export default function ProductCard({product}:ProductCardProps){
                   alt={product.description}
                   width={800}
                   height={400}
+                  className='h-48 object-cover'
                />
             </figure>
             <div className='card-body '>
                <h2 className='card-title'> 
                   {product.name}
+                  {isNew && <div className='badge badge-secondary'>NEW</div>}
                </h2>
                <p>{product.description}</p>
                <PriceTag price={product.price}/>
