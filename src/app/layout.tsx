94573bbce8312aa9f3e69819b18fb74d059fd722
shafi <shafi.bahrami.2015@gmail.com>
2023-10-31 21:26:42 +0100

    mdb types for user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer'
+import SessionProvider  from './SessionProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,11 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        <main className='p-4 min-w-[300px] max-w-7xl m-auto'>
-          {children}
-        </main>
-        <Footer/>
+        <SessionProvider>
+           <Navbar/>
+           <main className='p-4 min-w-[300px] max-w-7xl m-auto'>
+              {children}
+           </main>
+           <Footer/>
+        </SessionProvider>
       </body>
     </html>
   )
